ce456bb4a4cba9c17a86c180daab48fe3508af72
Mikael Ståldal <mikael@staldal.nu>
2001-08-05 14:38:08 +0000

    Scripts for running Lagoon

---
+++ bin/buildsitemap.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Windows batch script for running Lagoon
+
+rem SET LAGOON_HOME=D:\lagoon
+
+java -classpath %CLASSPATH%;%LAGOON_HOME%/lib/lagoon.jar;%LAGOON_HOME%/lib/xtree.jar nu.staldal.lagoon.BuildSitemap %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ bin/lagoon.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+rem OS/2 batch script for running Lagoon
+
+rem SET LAGOON_HOME=D:\lagoon
+
+java -DresourceDir=%LAGOON_HOME%\resource -classpath %CLASSPATH%;%LAGOON_HOME%\lib\lagoon.jar;%LAGOON_HOME%\lib\lsp.jar;%LAGOON_HOME%\lib\xtree.jar nu.staldal.lagoon.LagoonCLI %1 %2 %3 %4 %5 %6 %7 %8 %9
