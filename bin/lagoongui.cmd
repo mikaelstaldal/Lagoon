e72e27816c950a02c41153d881a723b62a787f55
Mikael Ståldal <mikael@staldal.nu>
2001-12-27 20:12:34 +0000

    LagoonGUI

---
+++ bin/lagoongui.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+rem OS/2 batch script for running Lagoon GUI
+
+rem SET LAGOON_HOME=D:\lagoon
+
+java -classpath %CLASSPATH%;%LAGOON_HOME%\lib\lagoon.jar;%LAGOON_HOME%\lib\xmlutil.jar nu.staldal.lagoon.LagoonGUI %1 %2 %3 %4 %5 %6 %7 %8 %9
