53ae30d33398ad1e793d3d2add1d76dd8c529bf3
Mikael Ståldal <mikael@staldal.nu>
2001-12-27 21:13:57 +0000

    Removed lib directory

--- bin/lagoongui.bat
+++ bin/lagoongui.bat
@@ -4,4 +4,4 @@ rem Windows batch script for running Lagoon GUI
 
 rem SET LAGOON_HOME=D:\lagoon
 
-java -classpath %CLASSPATH%;%LAGOON_HOME%\lib\lagoon.jar;%LAGOON_HOME%\lib\xmlutil.jar nu.staldal.lagoon.LagoonGUI %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %CLASSPATH%;%LAGOON_HOME%\lagoon.jar;%LAGOON_HOME%\xmlutil.jar nu.staldal.lagoon.LagoonGUI %1 %2 %3 %4 %5 %6 %7 %8 %9
