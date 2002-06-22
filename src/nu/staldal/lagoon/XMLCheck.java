efadc0ba53fce60bab4adc5798d5b57e5616b47c
Mikael Ståldal <mikael@staldal.nu>
2002-06-22 14:47:36 +0000

    foo

--- src/nu/staldal/lagoon/XMLCheck.java
+++ src/nu/staldal/lagoon/XMLCheck.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2001, Mikael Ståldal
- * All rights reserved.
+ * All rights reserved..
  * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions 
