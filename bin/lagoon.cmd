530fac2e7f8ffcb96ff7ae884865fe57029fe136
Mikael Ståldal <mikael@staldal.nu>
2004-05-12 14:19:27 +0000

    Removed lsp.jar

--- bin/lagoon.cmd
+++ bin/lagoon.cmd
@@ -4,4 +4,4 @@ rem Windows and OS/2 batch script for running Lagoon
 
 rem SET LAGOON_HOME=D:\lagoon
 
-java -classpath %CLASSPATH%;%LAGOON_HOME%\lagoon.jar;%LAGOON_HOME%\xmlutil.jar;%LAGOON_HOME%\lsp.jar nu.staldal.lagoon.LagoonCLI %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %CLASSPATH%;%LAGOON_HOME%\lagoon.jar;%LAGOON_HOME%\xmlutil.jar nu.staldal.lagoon.LagoonCLI %1 %2 %3 %4 %5 %6 %7 %8 %9
